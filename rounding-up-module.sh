f4096e2d80c63d245f3905f1d1aa9d0dd4a2be55
bveldhuyzen <77223171+bveldhuyzen@users.noreply.github.com>
2021-12-10 04:18:08 +0000

    Update rounding-up-module.sh
    /up

--- rounding-up-module.sh
+++ rounding-up-module.sh
@@ -1,5 +1,5 @@
 #!bin/bash
-#Rounds any number up to the nearest (multiple of) any specified decimal(s).
+#Rounds any number up to the nearest (multiple of) specified decimal(s).
 #
 #github.com/bveldhuyzen
 #2021
